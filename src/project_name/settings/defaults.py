ff75fa2b6ae10bfbae094ccf60764e8cfc8df76b
Adam Meszaros <adam.meszaros.it@gmail.com>
2014-04-23 23:05:50 +0100

    firefox webfont mimetype fix

--- src/project_name/settings/defaults.py
+++ src/project_name/settings/defaults.py
@@ -6,6 +6,15 @@ environments should be here, like applications.
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+#######################################################
+#
+# Webfont mime type fix for firefox and safari
+#
+#######################################################
+
+import mimetypes
+mimetypes.add_type("application/font-woff", ".woff", True)
+
 
 ####################################################
 # Applications                                     #
