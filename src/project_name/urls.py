4db15c3202824514b84006e3c076ce7192dd9988
Oscar Carballal Prego <info@oscarcp.com>
2014-07-01 17:15:51 +0100

    Push fix for the yawd admin to work straight out of the box

--- src/project_name/urls.py
+++ src/project_name/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from yawdadmin import admin_site
+
 admin.autodiscover()
+
+# Yawd admin needs to rewrite the whole discovery, so instead of going one by
+# one in the admins, we call these which rewrites the app registry into Yawd
+admin_site._registry.update(admin.site._registry)
 
 ####################################################
 # Default URL patterns, you shouldn't modify this  #
@@ -16,7 +22,7 @@ sitemaps = {
 urlpatterns = patterns('',
 
     # url(r'^$', '{{ project_name }}.views.home', name='home'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin_site.urls)),
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps})
 )
 
